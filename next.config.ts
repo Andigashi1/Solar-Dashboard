62595cff2922b0686297c6926dd21b37dd9f5746
Andi <andig2413@gmail.com>
2026-05-13 14:53:02 +0200

    Initial Commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: __dirname,
+  },
+
+   images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "apod.nasa.gov",
+      },
+      {
+        protocol: "https",
+        hostname: "www.youtube.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
